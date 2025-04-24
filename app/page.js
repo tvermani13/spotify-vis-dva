244970fcec26e441b639d7b536de528da6da108c
Tejas Vermani <tejasvermani@gmail.com>
2025-04-24 16:01:39 -0400

    minor ESLINT changes

--- app/page.js
+++ app/page.js
@@ -12,17 +12,17 @@ export default function Choropleth() {
         <div className="flex items-center justify-center min-h-[60vh]">
           <div className="max-w-2xl text-center">
             <div className="text-text-secondary text-2xl">
-              Welcome to Team Mark's "Exploring Global Music Trends" Project. We
-              will build an interactive application showcasing Spotify and
-              country related data to reveal global music trends. Using a
-              dataset of top Spotify songs by country with features like
-              danceability and popularity and datasets on economic and social
-              factors, we will explore song popularity by country, determine
-              correlations between song characteristics and country data, and
-              cluster similar nations. We intend to create a novel three-tier
-              geographic, and chord chart and heat map visuals. Additionally, we
-              plan to globally display potential popularity (graded choropleth)
-              based on user-selected song characteristics.
+              Welcome to Team Mark&apos;s &quot;Exploring Global Music
+              Trends&quot; Project. We will build an interactive application
+              showcasing Spotify and country related data to reveal global music
+              trends. Using a dataset of top Spotify songs by country with
+              features like danceability and popularity and datasets on economic
+              and social factors, we will explore song popularity by country,
+              determine correlations between song characteristics and country
+              data, and cluster similar nations. We intend to create a novel
+              three-tier geographic, and chord chart and heat map visuals.
+              Additionally, we plan to globally display potential popularity
+              (graded choropleth) based on user-selected song characteristics.
             </div>
           </div>
         </div>
